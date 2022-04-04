1c979cd0ce87ea069440450fc837bf1719b0a93d
irinakorobeinikova <irinakor81@mail.ru>
2022-04-05 01:47:52 +0200

    add about method

--- src/lessons/Lesson4_1.java
+++ src/lessons/Lesson4_1.java
@@ -2,10 +2,20 @@ package lessons;
 
 public class Lesson4_1 {
 
-    public static void main(String[] args) {
-
+    public static void m() {
         System.out.println("Hello");
+    }
 
+    public static void m1(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        m();
+        m1(2, 3);
 
     }
+
+
 }
