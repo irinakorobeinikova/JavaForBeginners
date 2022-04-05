b694db82fb942489b46a33fe1ed27bf154ea445f
irinakorobeinikova <irinakor81@mail.ru>
2022-04-05 02:19:18 +0200

    add about method signature

--- src/lessons/Lesson4_1.java
+++ src/lessons/Lesson4_1.java
@@ -11,9 +11,46 @@ public class Lesson4_1 {
         System.out.println(sum);
     }
 
+    public static void m1(int a, int b, int c) {
+        int division = a / b;
+        System.out.println(division);
+    }
+
+    public static void m2() {
+        int a = 5;
+        int b = 1;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static void printTaskTitle() {
+        int a = 0;
+        a ++;
+        System.out.println("Task " + a);
+        System.out.println("=================================");
+    }
+
+    public static void mPrint(int m) {
+        System.out.println(m);
+    }
+    public static void mPrint(String f) {
+        System.out.println(f);
+    }
+    public static void mPrint(char c) {
+        System.out.println(c);
+    }
+
     public static void main(String[] args) {
         m();
         m1(2, 3);
+        m1(8, 2, 3);
+        m2();
+        printTaskTitle();
+
+        mPrint(10);
+        mPrint("Hello");
+        mPrint('a');
+
 
     }
 
