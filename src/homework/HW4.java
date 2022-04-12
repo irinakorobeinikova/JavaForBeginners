0f0fcf0c52879f0852a80c8343e308483bd49fd0
irinakorobeinikova <irinakor81@mail.ru>
2022-04-12 02:56:12 +0200

    add HW4.Task14

--- src/homework/HW4.java
+++ src/homework/HW4.java
@@ -8,8 +8,9 @@ public class HW4 {
 
     public static void numberTask() {
 
-        System.out.println("\u001B[33m" + "*****************" + "\u001B[35m" + "Task "
-                + number + "\u001B[33m" + "*****************" + "\u001B[0m");
+        System.out.println();
+        System.out.println("\u001B[33m" + "**************************" + "\u001B[35m" + "Task "
+                + number + "\u001B[33m" + "**************************" + "\u001B[0m");
 
         number++;
         System.out.println();
@@ -18,6 +19,7 @@ public class HW4 {
     public static void space() {
 
         System.out.println();
+        System.out.println("\u001B[33m" + "***********************************************************" + "\u001B[0m");
     }
 
     public static int subItem = 1;
@@ -29,74 +31,84 @@ public class HW4 {
         subItem++;
     }
 
+    public static int devResult;
+    public static int devBack;
+
+    public static void compareMethod(int firstNum, int secondNum) {
+
+        devResult = firstNum / secondNum;
+        devBack = firstNum % secondNum;
+
+        System.out.println("The result of dividing " + firstNum + " by " + secondNum + " = "
+        + devResult + " and the remainder of the division = " + devBack);
+    }
+
     public static void main(String[] args){
 
         numberTask();
-        numberTask();
-        numberTask();
+        /**In the package, create class HW4 homework, do all the work in this class
+         */
 
-        System.out.println("(2 = 2) AND (7 = 7)");
-        System.out.println((2 == 2) && (7 == 7));
-        space();
-        System.out.println("NOT(15 < 3);");
-        System.out.println(!(15 < 3));
-        space();
-        System.out.println("(\"Pine\" = \"Oak\") OR (\"Cherry\" = \"Maple\")");
-        System.out.println(("Pine" == "Oak") || ("Cherry" == "Maple"));
-        space();
-        System.out.println("Not(\"Pine\" = \"Oak\")");
-        System.out.println(!("Pine" == "Oak"));
-        space();
-        System.out.println("(NOT(15 < 3)) AND (10 > 20)");
-        System.out.println((!(15 < 3)) && (10 > 20));
-        space();
+        numberTask();
+        /**Write a method that will print the task number before each answer. Come up with your own design.
+         */
+
+        numberTask();
+        /**Write the following logical expressions in code:
+         * (2 = 2) AND (7 = 7)
+         * NOT(15 < 3)
+         * ("Pine" = "Oak") OR ("Cherry" = "Maple")
+         * NOT("Pine" = "Oak")
+         * NOT(15 < 3) AND (10 > 20)
+         * "Eyes are given to see AND the third floor is under the second floor"
+         * (6 / 2 = 3) OR (7 * 5 = 20)
+         */
 
         String eyes = "to see";
-
         int floor2 = 2;
         int floor3 = 3;
 
-        System.out.println("(\"Eyes are given to see\") AND (\"Under the third floor is the second floor\")");
+        System.out.println((2 == 2) && (7 == 7));
+        System.out.println(!(15 < 3));
+        System.out.println(("Pine" == "Oak") || ("Cherry" == "Maple"));
+        System.out.println(!("Pine" == "Oak"));
+        System.out.println((!(15 < 3)) && (10 > 20));
         System.out.println((eyes.equals("to see")) && (floor2 == floor3 - 1));
-        space();
-        System.out.println("(6 / 2 = 3) OR (7 * 5 = 20)");
         System.out.println((6 / 2 == 3) || (7 * 5 == 20));
-        space();
 
         numberTask();
+        /**Write as code:
+         * ("There are 70 seconds in a minute") OR ("The working clock shows the time")
+         * "!(28 > 7) AND !(300/5 = 60)"
+         * ("TV - electrical appliance") AND ("Glass - wood")
+         * "Not((300 < 100)) → ("Thirst can be quenched with water")"
+         * "(75 < 81) → (88 = 88)"
+         */
 
         int sec = 60;
         int param = 70;
         String watchTime = "time";
-        System.out.println("(\"There are 70 seconds in a minute\") OR (\"The working clock shows the time\")");
-        System.out.println(sec == param || watchTime.equals("time"));
-        space();
-
-        System.out.println("!(28 > 7) И !(300/5 = 60)");
-        System.out.println(!(28 > 7) && !(300 / 5 == 60));
-        space();
-
         String tv = "electrical appliance";
         String glass = "Glass";
         String wood = "wood";
 
-        System.out.println("(\"TV - electrical appliance\") AND (\"Glass - wood\");");
+        System.out.println(sec == param || watchTime.equals("time"));
+        System.out.println(!(28 > 7) && !(300 / 5 == 60));
         System.out.println((tv.equals("electrical appliance")) && (glass == wood));
-        space();
 
-        System.out.println("Not((300 < 100)) → (\"Thirst can be quenched with water\")");
         if (!(300 < 100)) {
-            System.out.println("(\"Thirst can be quenched with water\")");
+            System.out.println("Thirst can be quenched with water");
         }
-        space();
-
-        System.out.println("(75 < 81) → (88 = 88)");
         if (75 < 81) {
-            System.out.println(88 == 88);
+            System.out.println("88 = 88");
         }
-        space();
 
         numberTask();
+        /**Write as code:
+         * a) Andrei is older than Svetlana. Natasha is older than Svetlana.
+         * b) There are textbooks on the shelf and catalogs on the table.
+         * c) Most of the children are girls. The rest are boys.
+         */
 
         int ageSvetlana = 22;
         int ageAndrey = 38;
@@ -105,7 +117,6 @@ public class HW4 {
         if ((ageAndrey > ageSvetlana) && (ageNata > ageSvetlana)) {
             System.out.println("Andrey is older than Svetlana. Nata is older than Svetlana.");
         }
-        space();
 
         String shelf = "books";
         String table = "catalogs";
@@ -117,7 +128,6 @@ public class HW4 {
         if (table.equals("catalogs")) {
             System.out.println("there are catalogs on the table.");
         }
-        space();
 
         int all = 100;
         int girls = 72;
@@ -128,33 +138,40 @@ public class HW4 {
         } else {
             System.out.println("false");
         }
-        space();
 
         numberTask();
+        /**Write as code:
+         * "You can only get a driver's license when you turn 16"
+         */
 
         int ageDrive = 16;
 
         if (ageDrive >= 16 && ageDrive < 150) {
-            System.out.println("\"You can only get a driver's license when you turn 16.\"");
+            System.out.println("\"You can only get a driver's license when you turn 16\"");
         } else {
             System.out.println("false");
         }
-        space();
 
         numberTask();
+        /**Write as code:
+         * "Peter does not ride the bus, but at the same time he reads a book or does not look out of the window"
+         */
 
         boolean ridePeter = false;
         boolean readPeter = true;
         boolean lookPeter = false;
+
         if (ridePeter && (readPeter || lookPeter)) {
             System.out.println("false");
         } else  {
             System.out.println("\"Peter does not ride the bus, but at the same time "
-                    + "he reads a book or does not look out the window\"");
+                    + "he reads a book or does not look out of the window\"");
         }
-        space();
 
         numberTask();
+        /**Write as code:
+         * "If you are with a friend, it's good, but when it's the other way around, it's bad."
+         */
 
         String friends = "with friend";
 
@@ -163,12 +180,21 @@ public class HW4 {
         } else {
             System.out.println("\"but when it's the other way around, it's bad.\"");
         }
-        space();
 
         numberTask();
+        /**Write expressions as if-else conditions:
+         * "If you are over 18, then you are an adult. Otherwise, you are a child."
+         * "If the ground is dry, then there is no rain. If the ground is wet, then it is raining."
+         * "If the ground is dry, then there is no rain. If the ground is wet, then it is raining.
+         * Otherwise it's snowing."
+         * "If there are clouds in the sky, it is raining, otherwise it is “a monkey's wedding”."
+         * "If today is Saturday, then tomorrow is Sunday. If today is Friday, then yesterday was Thursday.
+         * Otherwise, yesterday was not Thursday, and tomorrow is not Sunday."
+         * "If a crab whistled on the mountain, then eternity has passed, otherwise you should wait farther."
+         * "If you are 18 or you have finished school, then you do not have to live with your parents,
+         * otherwise live with your parents."
+         */
         subItemTask();
-
-        System.out.println("\"If you are over 18, then you are an adult. Otherwise, you are a child.\"");
 
         int yourAge = 18;
 
@@ -177,12 +203,8 @@ public class HW4 {
         } else {
             System.out.println("You are a child");
         }
-        space();
 
         subItemTask();
-
-        System.out.println("\"If the ground is dry, then there is no rain. "
-                + "If the ground is wet, then it is raining.\"");
 
         String groundDry = "dry";
 
@@ -191,12 +213,8 @@ public class HW4 {
         } else {
             System.out.println("It is raining");
         }
-        space();
 
         subItemTask();
-
-        System.out.println("\"If the ground is dry, then there is no rain. If the ground is wet, then it is raining."
-                + " Otherwise it's snowing.\"");
 
         String groundWet = "wet";
 
@@ -208,11 +226,8 @@ public class HW4 {
         } else {
             System.out.println("It's snowing");
         }
-        space();
 
         subItemTask();
-
-        System.out.println("\"If there are clouds in the sky, it is raining, otherwise it is “ a monkey's wedding”.\"");
 
         String sky = "clouds";
 
@@ -221,13 +236,8 @@ public class HW4 {
         } else {
             System.out.println("It is “a monkey's wedding\"");
         }
-        space();
 
         subItemTask();
-
-        System.out.println("\"If today is Saturday, then tomorrow is Sunday. "
-                + "If today is Friday, then yesterday was Thursday. "
-                + "Otherwise, yesterday was not Thursday, and tomorrow is not Sunday.\"");
 
         String dayOfWeek1 = "Saturday";
         String dayOfWeek2 = "Friday";
@@ -240,12 +250,8 @@ public class HW4 {
         } else {
             System.out.println("Yesterday was not Thursday, and tomorrow is not Sunday");
         }
-        space();
 
         subItemTask();
-
-        System.out.println("\"If a crab whistled on the mountain, then eternity has passed, "
-                + "otherwise you should wait farther.\"");
 
         String crab = "whistle";
 
@@ -254,12 +260,8 @@ public class HW4 {
         } else {
             System.out.println("Wait farther");
         }
-        space();
 
         subItemTask();
-
-        System.out.println("\"If you are 18 or you have finished school, "
-                + "then you do not have to live with your parents, " + "otherwise live with your parents.\"");
 
         int ageTeenager = 18;
         String school = "finished";
@@ -269,12 +271,11 @@ public class HW4 {
         } else {
             System.out.println("Live with your parents");
         }
-        space();
 
         numberTask();
-
-        System.out.println("Check the number for evenness. "
-                + "If the number is even, double that number, otherwise square the number.");
+        /**Check the number for evenness. If the number is even, double the number, otherwise square the number.
+         *
+         */
 
         int checkNumber = 32;
 
@@ -283,14 +284,13 @@ public class HW4 {
         } else {
             System.out.println(checkNumber * checkNumber);
         }
-        space();
 
         numberTask();
-
-        System.out.println("Write an algorithm for checking the age for compliance with the conditions (if-else):\n"
-                + "You can vote from the age of 18\n"
-                + "You can drive a car from the age of 16\n"
-                + "You can go to school from the age of 5");
+        /**Write an algorithm for checking the age for compliance with the conditions (if-else):
+         * "You can vote from the age of 18"
+         * "You can drive a car from the age of 16"
+         * "You can go to school from the age of 5"
+         */
 
         int voteAge = 18;
         int driveAge = 16;
@@ -306,15 +306,13 @@ public class HW4 {
             System.out.println("You can go to school");
         }
 
-        space();
-
         numberTask();
-
-        System.out.println("Check a student's grade and performs the following actions:\n" +
-                "5 - issue a commendable letter and go to the next class\n" +
-                "4 - go to the next class\n" +
-                "3 - give a task for the summer and go to the next class\n" +
-                "2 - call parents and leave in the current class for the second year");
+        /**"Check a student's grade and performs the following actions:
+         * 5 - issue a commendable letter and go to the next class
+         * 4 - go to the next class
+         * 3 - give a task for the summer and go to the next class
+         * 2 - call parents and leave in the current class for the second year
+         */
 
         int grade = 4;
 
@@ -328,17 +326,15 @@ public class HW4 {
             System.out.println("Call parents and leave in the current class for the second year");
         }
 
-        space();
-
         numberTask();
-
-        System.out.println("Write an algorithm for a program that checks 2 numbers. "
-                        + "The program adds numbers that are divisible by 3 without a remainder, "
-                        + "and subtracts numbers that are divisible by 5 without a remainder. "
-                        + "The program multiplies numbers that are divisible by 2 without a remainder, "
-                        + "but if at least one number is negative, the program multiplies the result of "
-                        + "the previous action by (-1). If the numbers have not passed any checks, "
-                        + "the program prints an error about the impossibility to perform actions.");
+        /**Write an algorithm for a program that checks 2 numbers.
+         * The program adds numbers that are divisible by 3 without a remainder,
+         * and subtracts numbers that are divisible by 5 without a remainder.
+         * The program multiplies numbers that are divisible by 2 without a remainder,
+         * but if at least one number is negative, the program multiplies the result of the previous action by (-1).
+         * If the numbers have not passed any checks, the program prints an error about the impossibility
+         * of performing actions.
+         */
 
         int digit1 = 15;
         int digit2 = 15;
@@ -358,6 +354,21 @@ public class HW4 {
                 && (!(digit1 % 2 == 0 && digit2 % 2 == 0))) {
                 System.out.println("Impossible to perform actions");
         }
+
+        numberTask();
+
+        int k = 5;
+        int l = 10;
+        int m = 15;
+
+        compareMethod(k, l);
+        compareMethod(k, m);
+        compareMethod(l, m);
+        compareMethod(m, k);
+        space();
+
+
+
 
 
 
