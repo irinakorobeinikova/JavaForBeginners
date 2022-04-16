8f24852de79ace95d6b5f9040f2374ab70cf432b
irinakorobeinikova <irinakor81@mail.ru>
2022-04-16 02:37:13 +0200

    add a new Class 'Lesson5'

---
+++ src/lessons/Lesson5.java
@@ -0,0 +1,27 @@
+package lessons;
+
+public class Lesson5 {
+
+    public static void main(String[] args) {
+
+        /** Given 3 numbers.
+         * Need to find the most accurate difference
+         * between the minimum number and the average value.
+         * If the difference is greater than 3.51,
+         * show the user the message "Big spread of numbers",
+         * otherwise show the message "Small spread of numbers"
+         */
+
+        int a = 5;
+        int b = 2;
+        int c = 10;
+
+
+
+
+
+
+
+
+    }
+}
