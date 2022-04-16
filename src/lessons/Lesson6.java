81c28b7f227198565e4eb4eb3fba4858859a46ab
irinakorobeinikova <irinakor81@mail.ru>
2022-04-17 01:37:53 +0200

    add Lesson6.Task1

--- src/lessons/Lesson6.java
+++ src/lessons/Lesson6.java
@@ -1,6 +1,24 @@
 package lessons;
 
 public class Lesson6 {
+    public static void main(String[] args) {
 
+//        for(; ;) {
+//
+//        }
+/**1 Print for all numbers from 1 to 5 inclusive.
+ *
+ */
+        for(int i = 1; i < 6; i++) {
+            System.out.println("Java for beginners");
+        }
 
+/**2 Print for all numbers from 1 to 5 inclusive/
+ *
+ */
+         for() {
+
+         }
+
+    }
 }
