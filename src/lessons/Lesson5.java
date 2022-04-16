4c77aebd9558571b733550c205584d7f19db0925
irinakorobeinikova <irinakor81@mail.ru>
2022-04-17 01:14:16 +0200

    add HW4.Lesson5.Task1

--- src/lessons/Lesson5.java
+++ src/lessons/Lesson5.java
@@ -18,6 +18,40 @@ public class Lesson5 {
         return min;
     }
 
+    public static double average(int a, int b, int c) {
+
+        return ((double)a + b +c) / 3;
+    }
+
+    public static double returnAbc(double number) {
+        if (number < 0) {
+            number = (-1) * number;
+        }
+
+        return number;
+    }
+
+    public static double countOfDifferenceBetweenMinAndAverage(int a, int b, int c) {
+        int min = returnMinOfThree(a, b, c);
+        double average = average(a, b, c);
+
+        return returnAbc(min - average);
+    }
+
+    public static String displayMessage(int a, int b, int c) {
+
+        String message;
+
+        if (countOfDifferenceBetweenMinAndAverage(a, b, c) > 3.51) {
+            message = "Big spread of numbers";
+            System.out.println(message);
+        } else {
+            message = "Small spread of numbers";
+            System.out.println(message);
+        }
+
+        return message;
+    }
 
     public static void main(String[] args) {
 
@@ -34,9 +68,7 @@ public class Lesson5 {
         int c = 10;
 
         System.out.println(returnMinOfThree(a, b, c));
-
-
-
-
+        System.out.println(average(a, b, c));
+        displayMessage(a, b, c);
     }
 }
