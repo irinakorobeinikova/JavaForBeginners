3063b37c107b23290a1ba023474520aefe94158c
irinakorobeinikova <irinakor81@mail.ru>
2022-04-16 03:12:28 +0200

    add Lesson5.Task1

--- src/lessons/Lesson5.java
+++ src/lessons/Lesson5.java
@@ -2,9 +2,26 @@ package lessons;
 
 public class Lesson5 {
 
+    public static int returnMinOfThree(int a, int b, int c) {     // Method (Task #1)
+
+        int min;
+
+        if (a < b) {
+            min = a;
+        } else {
+            min = b;
+        }
+        if (min > c) {
+            min = c;
+        }
+
+        return min;
+    }
+
+
     public static void main(String[] args) {
 
-        /** Given 3 numbers.
+        /**1 Given 3 numbers.
          * Need to find the most accurate difference
          * between the minimum number and the average value.
          * If the difference is greater than 3.51,
@@ -16,9 +33,7 @@ public class Lesson5 {
         int b = 2;
         int c = 10;
 
-
-
-
+        System.out.println(returnMinOfThree(a, b, c));
 
 
 
