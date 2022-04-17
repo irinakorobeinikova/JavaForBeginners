f6a7cb4e549ac8a4a5ba32eacde6c0e29a275704
irinakorobeinikova <irinakor81@mail.ru>
2022-04-17 02:04:13 +0200

    add Lesson6.Task3

--- src/lessons/Lesson6.java
+++ src/lessons/Lesson6.java
@@ -1,24 +1,66 @@
 package lessons;
 
 public class Lesson6 {
+
+    public static int number = 1;
+
+    public static void numberTask() {
+
+        System.out.println();
+        System.out.println("\u001B[33m" + "**************************" + "\u001B[35m" + "Task "
+                + number + "\u001B[33m" + "**************************" + "\u001B[0m");
+
+        number++;
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
 //        for(; ;) {
 //
 //        }
-/**1 Print for all numbers from 1 to 5 inclusive.
- *
- */
+        /**1 Print for all numbers from 1 to 5 inclusive.
+         *
+         */
+        numberTask();
+
         for(int i = 1; i < 6; i++) {
             System.out.println("Java for beginners");
         }
 
-/**2 Print all numbers from 1 to 5 inclusive.
- *
- */
-         for(int i = 1; i < 6; i++) {
+        /**2 Print all numbers from 1 to 5 inclusive.
+         *
+         */
+        numberTask();
+
+        for(int i = 1; i < 6; i++) {
              System.out.println(i);
-         }
+        }
+
+        /**2 Print all numbers from 1 to 5 inclusive.
+         *
+         */
+        numberTask();
+
+        for(int i = 1; i < 6; i++) {
+            System.out.println(i);
+        }
+
+        /**3 Print only odd numbers from 1 to 10 inclusive.
+         *
+         */
+        numberTask();
+
+        for (int i = 1; i < 11; i = i + 2) {
+                System.out.println(i);
+        }
+
+
+
+
+
+
+
 
 
 
