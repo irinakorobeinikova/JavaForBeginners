26ee74b9e88806615f9bd60047b0d2cf6592095e
irinakorobeinikova <irinakor81@mail.ru>
2022-04-18 01:16:01 +0200

    add HW5.Task2

--- src/homework/HW5.java
+++ src/homework/HW5.java
@@ -2,7 +2,37 @@ package homework;
 
 public class HW5 {
 
-    public static void main(String[] args) {
+    public static int number = 2;
+
+    public static void numberTask() {
+
+        System.out.println();
+        System.out.println("\u001B[33m" + "**************************" + "\u001B[35m" + "Task "
+                + number + "\u001B[33m" + "**************************" + "\u001B[0m");
+
+        number++;
+        System.out.println();
+    }
+
+    //Task 2
+    public static void verifyEquals(int expectedResult, int actualResult) {
+
+        if (expectedResult == actualResult) {
+            System.out.println("\u001B[32m" + "Pass" + "\u001B[0m");
+        } else {
+            System.out.println("\u001B[31m" + "Fail" + "\u001B[0m");
+        }
+    }
+
+    public static void verifyEquals(String expectedResult, String actualResult) {
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("\u001B[32m" + "Pass" + "\u001B[0m");
+        } else {
+            System.out.println("\u001B[31m" + "Fail" + "\u001B[0m");
+        }
+    }
+
 
 
 
