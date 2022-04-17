663739b4918ab5d0fe9c813072da5da18a3f8436
irinakorobeinikova <irinakor81@mail.ru>
2022-04-18 00:23:03 +0200

    add Lesson 5.Task7

--- src/lessons/Lesson6.java
+++ src/lessons/Lesson6.java
@@ -19,7 +19,7 @@ public class Lesson6 {
 //        for(; ;) {
 //
 //        }
-        /**1 Print all numbers from 1 to 5 inclusive.
+        /**1 Print "Java for beginners" for all numbers from 1 to 5 inclusive.
          *
          */
         numberTask();
@@ -82,15 +82,36 @@ public class Lesson6 {
             System.out.println(i);
         }
 
+        /**7 Print the song "Lyrics of the song 99 Bottles of Beer".
+         *
+         */
+        numberTask();
 
+        String bottles = " bottles of beer";
+        String bottle = " bottle of beer";
+        String wall = " on the wall";
+        String take = "Take one down and pass it around";
+        String commaSpace = ", ";
+        String dot = ".";
+        String go = "Go to the store and buy some more";
+        String noMoreC = "No more";
+        String noMoreL = "no more";
+        String ln = "\n";
 
-
-
-
-
-
-
-
-
+        for(int i = 99; i > -1; i--) {
+            if (i == 2) {
+                System.out.println(i + bottles + wall + commaSpace + i + bottles + dot);
+                System.out.println(take + commaSpace + (i - 1) + bottle +wall + dot + ln);
+            } else if (i == 1) {
+                System.out.println(i + bottle + wall + commaSpace + i + bottle + dot);
+                System.out.println(take + commaSpace + noMoreL + bottles +wall + dot + ln);
+            } else if (i == 0) {
+                System.out.println(noMoreC + bottles + wall + commaSpace + noMoreL + bottle + dot);
+                System.out.println(go + commaSpace + (i + 99) + bottles +wall + dot);
+            } else {
+                System.out.println(i + bottles + wall + commaSpace + i + bottles + dot);
+                System.out.println(take + commaSpace + (i - 1) + bottles +wall + dot + ln);
+            }
+        }
     }
 }
