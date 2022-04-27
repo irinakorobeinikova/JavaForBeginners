36c7155bf094801a31a3ad0f9ba1613731f43048
irinakorobeinikova <irinakor81@mail.ru>
2022-04-28 00:57:31 +0200

    add HW6.Task1

--- src/homework/HW6.java
+++ src/homework/HW6.java
@@ -1,4 +1,39 @@
 package homework;
 
 public class HW6 {
+
+    public static int number = 1;
+
+    public static void numberTask() {
+
+        System.out.println();
+        System.out.println("\u001B[33m" + "**************************" + "\u001B[35m" + "Task "
+                + number + "\u001B[33m" + "**************************" + "\u001B[0m");
+
+        number++;
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+
+        numberTask();
+        /**1 Print a sequence of numbers from 0 to 9 inclusive.
+         *
+         */
+
+        for (int i = 1; i < 10; i ++) {
+            System.out.println(i);
+        }
+
+        numberTask();
+
+
+
+
+
+
+
+
+    }
 }
