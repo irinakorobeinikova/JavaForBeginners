8f9cafad7c30036ea0d702f136a63d5baa6ab543
irinakorobeinikova <irinakor81@mail.ru>
2022-04-28 01:02:21 +0200

    add HW6Task2

--- src/homework/HW6.java
+++ src/homework/HW6.java
@@ -27,7 +27,22 @@ public class HW6 {
         }
 
         numberTask();
+        /**2 Print a sequence of numbers from 10 exclusively to 0 inclusive.
+         *
+         */
 
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        numberTask();
+        /**3 Print a sequence of numbers from 50 to 55 inclusive,
+         *  with step 2.
+         */
+
+        for (int i = 50; i <= 55; i += 2) {
+            System.out.println(i);
+        }
 
 
 
