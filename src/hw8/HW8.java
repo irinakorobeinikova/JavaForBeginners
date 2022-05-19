caee2b53a0774670f18c81cfaf61585821838a16
irinakorobeinikova <irinakor81@mail.ru>
2022-05-20 00:24:09 +0200

    HW8Task6

--- src/hw8/HW8.java
+++ src/hw8/HW8.java
@@ -21,6 +21,23 @@ public class HW8 {
         boolean regWeight3 = regWeight.equals(HW8_1.regWeight1);
         boolean regName3 = regName.equals(HW8_1.regName1);
 
+        numberTask(6);
+        /** Compare variables specific to HW8_1 and HW8 classes
+         * and print the comparison results in a table.
+         */
+
+        line();
+        System.out.println("| HW8                     | " + "HW8_1                    | " + "areEquals |");
+        line();
+        System.out.println("| Integer regNum = " + regNum + "     | " + "Integer regNum1 = "
+                + HW8_1.regNum1 + "     | "  + regNum3 + "      |");
+        line();
+        System.out.println("| Double regWeight = " + regWeight + " | " + "Double regWeight1 = "
+                + HW8_1.regWeight1 + " | "  + regWeight3 + "      |");
+        line();
+        System.out.println("| String regName = " + regName + "  | " + "String regName1 = "
+                + HW8_1.regName1 + "  | "  + regName3 + "      |");
+        line();
 
 
 
