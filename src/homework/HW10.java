13e9ce0df5203cd26d525ecd584700762b9b6da8
irinakorobeinikova <irinakor81@mail.ru>
2022-06-01 01:19:00 +0200

    add HW10

---
+++ src/homework/HW10.java
@@ -0,0 +1,22 @@
+package homework;
+
+/** Write a CapitalizeWords algorithm that accepts as input
+ * sentence with a single space between words,
+ * and returns a sentence in which all words are capitalized
+ *
+ */
+
+public class HW10 {
+
+    public static String capitalizeWords(String sentence) {
+
+        if (sentence != null && sentence.length() != 0) {
+
+        }
+
+        return "";
+
+    }
+
+
+}
