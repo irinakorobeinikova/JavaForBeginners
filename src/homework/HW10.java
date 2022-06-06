ed13a706b1f2c6358ff4325e3a7a139f6e9d5392
irinakorobeinikova <irinakor81@mail.ru>
2022-06-06 02:30:57 +0200

    Add HW10, returnMinMaxAveArray

--- src/homework/HW10.java
+++ src/homework/HW10.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.Arrays;
+
 /** Write a CapitalizeWords algorithm that accepts as input
  * sentence with a single space between words,
  * and returns a sentence in which all words are capitalized
@@ -33,12 +35,52 @@ public class HW10 {
         return "";
     }
 
+    public static int[] minMaxAve(int[] array, int index1, int index2) {
+
+        if (array == null || array.length < 1 || index1 < 0 || index1 > index2 || index2 > array.length) {
+
+            return new int[] {};
+        }
+
+        int minValue = array[index1];
+        int maxValue = array[index2];
+        int aveValue = 0;
+        int sum = 0;
+
+        for (int i = index1; i <= index2; i++) {
+            if (array[index1] < array[i]) {
+                maxValue = array[i];
+            }
+            if (array[index1] > array[i]) {
+                minValue = array[i];
+            }
+            sum += array[i];
+        }
+
+        aveValue = sum / (index2 - index1 + 1);
+        int[] result = {minValue, maxValue, aveValue};
+
+        return result;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(capitalizeWords("           happy birthday!  "));
         System.out.println(capitalizeWords(""));
         System.out.println(capitalizeWords(null));
         System.out.println(capitalizeWords("     john jacob smith jr."));
         System.out.println(capitalizeWords(" "));
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int a = 2;
+        int b = 6;
+
+        System.out.println(Arrays.toString(minMaxAve(arr, a, b)));
+
+
+
+
+
 
 
     }
